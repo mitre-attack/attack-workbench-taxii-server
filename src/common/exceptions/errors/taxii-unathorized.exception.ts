9d1847e0aedc8c9268e6521558ab2a74bb8e5e9c
Sean Sica <23294618+seansica@users.noreply.github.com>
2022-06-15 13:14:44 -0400

    fixed mispelling in class name

--- src/common/exceptions/errors/taxii-unathorized.exception.ts
+++ src/common/exceptions/errors/taxii-unathorized.exception.ts
@@ -1,10 +1,10 @@
 import { TaxiiHttpErrorStatus } from "./helper";
 import { TaxiiErrorException } from "./interface/taxii-error.exception";
 
-export class TaxiiUnathorizedException extends TaxiiErrorException {
+export class TaxiiUnauthorizedException extends TaxiiErrorException {
   static readonly httpStatus?: number = TaxiiHttpErrorStatus.UNAUTHORIZED; // do not change
 
   constructor(props: Partial<TaxiiErrorException>) {
-    super(props, TaxiiUnathorizedException.httpStatus);
+    super(props, TaxiiUnauthorizedException.httpStatus);
   }
 }

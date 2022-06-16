bec96bf80098e7c5ebf15774db4bffce7a879dc2
Sean Sica <23294618+seansica@users.noreply.github.com>
2022-06-16 15:29:58 -0400

    implement unit test for workbench repo

---
+++ src/stix/providers/workbench/workbench.repository.spec.ts
@@ -0,0 +1,24 @@
+import { CacheModule } from "@nestjs/common";
+import { Test } from "@nestjs/testing";
+import { WorkbenchRepository } from "./workbench.repository";
+import { HttpModule } from "@nestjs/axios";
+import { TaxiiLoggerModule } from "src/common/logger/taxii-logger.module";
+import { TaxiiConfigModule } from "src/config";
+
+it("can create an instance of WorkbenchRepository", async () => {
+  const stixModule = await Test.createTestingModule({
+    imports: [
+      TaxiiConfigModule,
+      TaxiiLoggerModule,
+      HttpModule,
+      CacheModule.register({
+        isGlobal: true,
+        ttl: 600,
+      }),
+    ],
+    providers: [WorkbenchRepository],
+  }).compile();
+
+  const workbenchRepo = stixModule.get(WorkbenchRepository);
+  expect(workbenchRepo).toBeDefined();
+});

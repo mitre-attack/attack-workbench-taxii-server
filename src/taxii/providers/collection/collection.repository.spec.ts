56ea7e2081f0498cfdd3b19ddd41a2820d9f0855
Sean Sica <23294618+seansica@users.noreply.github.com>
2022-06-16 16:44:53 -0400

    formatted via prettier

---
+++ src/taxii/providers/collection/collection.repository.spec.ts
@@ -0,0 +1,29 @@
+import { Test } from "@nestjs/testing";
+import { StixModule } from "src/stix/stix.module";
+import { CollectionRepository } from "./collection.repository";
+import { TaxiiConfigModule } from "src/config";
+import { TaxiiLoggerModule } from "src/common/logger/taxii-logger.module";
+import { CacheModule } from "@nestjs/common";
+
+it("can create an instance of CollectionRepository", async () => {
+  const module = await Test.createTestingModule({
+    imports: [
+      TaxiiLoggerModule,
+      TaxiiConfigModule,
+      // CacheModule used by WorkbenchRepository
+      CacheModule.register({
+        isGlobal: true,
+        ttl: 600,
+      }),
+      // StixModule is used by CollectionRepository
+      StixModule.register({
+        useType: "workbench",
+      }),
+    ],
+    providers: [CollectionRepository],
+  }).compile();
+
+  const collectionRepo = module.get(CollectionRepository);
+  expect(collectionRepo).toBeDefined();
+
+});
--- src/taxii/providers/collection/collection.service.spec.ts
+++ src/taxii/providers/collection/collection.service.spec.ts
@@ -1,52 +1,47 @@
 import { Test } from "@nestjs/testing";
 import { CollectionService } from "./collection.service";
-import { CollectionRepository} from "./collection.repository";
+import { CollectionRepository } from "./collection.repository";
 import { TaxiiLoggerModule } from "src/common/logger/taxii-logger.module";
 import { TaxiiConfigModule } from "src/config";
 
-it('can create an instance of CollectionService', async () => {
+it("can create an instance of CollectionService", async () => {
+  // create a fake copy of the dependent services
+  const fakeCollectionRepo = {
+    findOne: (id: string) => {
+      // fake findOne returns a fake instance of CollectionDto
+      Promise.resolve({
+        id,
+        title: "fakeTitle",
+        description: "fakeDescription",
+        alias: "fakeAlias",
+        canRead: true,
+        canWrite: false,
+        mediaType: [],
+      });
+    },
+    findAll: () => {
+      // returns a fake (empty) instance of CollectionsDto
+      Promise.resolve({
+        collections: [],
+      });
+    },
+  };
 
-    // create a fake copy of the dependent services
+  // create test DI container
 
-    const fakeCollectionRepo = {
-        findOne: (id: string) => {
-            // fake findOne returns a fake instance of CollectionDto
-            Promise.resolve({
-                id,
-                title: "fakeTitle",
-                description: "fakeDescription",
-                alias: "fakeAlias",
-                canRead: true,
-                canWrite: false,
-                mediaType: []
-            });
-        },
-        findAll: () => {
-            // returns a fake (empty) instance of CollectionsDto
-            Promise.resolve({
-                collections: []
-            });
-        }
-    }
+  const module = await Test.createTestingModule({
+    imports: [TaxiiLoggerModule, TaxiiConfigModule],
+    providers: [
+      CollectionService,
+      {
+        provide: CollectionRepository,
+        useValue: fakeCollectionRepo,
+      },
+    ],
+  }).compile();
 
-    // create test DI container
+  // reach into DI container and get a copy of the dependency services
 
-    const module = await Test.createTestingModule({
-        imports: [
-            TaxiiLoggerModule,
-            TaxiiConfigModule
-        ],
-        providers: [
-            CollectionService,
-            {
-                provide: CollectionRepository,
-                useValue: fakeCollectionRepo
-            }
-        ]
-    }).compile();
-
-    // reach into DI container and get a copy of the dependency services
-
-    const collectionService = module.get(CollectionService);
-    expect(collectionService).toBeDefined();
+  const collectionService = module.get(CollectionService);
+  expect(collectionService).toBeDefined();
 });

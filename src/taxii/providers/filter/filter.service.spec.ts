110fe950457edfe3f96e3791d68069d64f9370af
Sean Sica <23294618+seansica@users.noreply.github.com>
2022-07-05 16:00:18 -0400

    formatting

--- src/taxii/providers/filter/filter.service.spec.ts
+++ src/taxii/providers/filter/filter.service.spec.ts
@@ -2,12 +2,12 @@ import { Test } from "@nestjs/testing";
 import { FilterService } from "./filter.service";
 import { TaxiiLoggerModule } from "src/common/logger/taxii-logger.module";
 
-it ("can create an instance of FilterService", async () => {
-    const module = await Test.createTestingModule({
-        imports: [TaxiiLoggerModule],
-        providers: [FilterService]
-    }).compile();
+it("can create an instance of FilterService", async () => {
+  const module = await Test.createTestingModule({
+    imports: [TaxiiLoggerModule],
+    providers: [FilterService],
+  }).compile();
 
-    const filterService = module.get(FilterService);
-    expect(filterService).toBeDefined();
+  const filterService = module.get(FilterService);
+  expect(filterService).toBeDefined();
 });

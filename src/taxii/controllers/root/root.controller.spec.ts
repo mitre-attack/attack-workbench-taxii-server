08445eb65296b7bd69edd4ec5c3f235f66df9d12
Sean Sica <23294618+seansica@users.noreply.github.com>
2022-07-06 11:10:51 -0400

    implemented unit test for root controller

--- src/taxii/controllers/root/root.controller.spec.ts
+++ src/taxii/controllers/root/root.controller.spec.ts
@@ -1,11 +1,15 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { RootController } from "./root.controller";
+import { DiscoveryModule } from "src/taxii/providers";
+import { TaxiiConfigModule } from "src/config";
+import { TaxiiLoggerModule } from "src/common/logger/taxii-logger.module";
 
 describe("RootController", () => {
   let controller: RootController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TaxiiConfigModule, TaxiiLoggerModule, DiscoveryModule],
       controllers: [RootController],
     }).compile();
 

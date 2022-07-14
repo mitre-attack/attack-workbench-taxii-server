6ed163058adaea8dd3f706e3f359cb93dbd385ed
Sean Sica <23294618+seansica@users.noreply.github.com>
2022-07-14 14:06:37 -0400

    added Jest E2E tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,40 +1,39 @@
 import * as request from "supertest";
 import { Test } from "@nestjs/testing";
 import { INestApplication } from "@nestjs/common";
-import {
-  CollectionService,
-  DiscoveryService,
-  EnvelopeService,
-  FilterService,
-  ManifestService,
-  ObjectService,
-  PaginationService,
-  VersionService,
-} from "src/taxii/providers";
-import { MockStixRepository } from "./mock-stix-repository";
-import { CollectionsController } from "../src/taxii/controllers/collections/collections.controller";
-import { HttpModule } from "@nestjs/axios";
+import { MockStixRepository } from "./mock-stix.repository";
 import { STIX_REPO_TOKEN } from "../src/stix/constants";
-import { TaxiiLoggerModule } from "../src/common/logger/taxii-logger.module";
 import { TaxiiConfigModule, TaxiiConfigService } from "../src/config";
-import { CollectionRepository } from "../src/taxii/providers/collection/collection.repository";
-import { ObjectRepository } from "../src/taxii/providers/object/object.repository";
-import { ManifestRecordService } from "../src/taxii/providers/manifest/manifest-record.service";
-import { DEFAULT_WORKBENCH_AUTH_HEADER } from "../src/config/defaults";
-import { RootController } from "../src/taxii/controllers/root/root.controller";
-import {
-  RequestContext,
-  RequestContextModule,
-} from "../src/common/middleware/request-context";
-import { WorkbenchRepository } from "../src/stix/providers/workbench/workbench.repository";
-import { TaxiiExceptionFilter } from "../src/common/exceptions/taxii-exception.filter";
 import { AppModule } from "../src/app.module";
+import { DiscoveryService } from "src/taxii/providers/discovery/discovery.service";
+import { EnvelopeDto } from "../src/taxii/providers";
+import * as util from "util";
+import { Response } from "supertest";
+import Expect = jest.Expect;
+
+function formatError(err: Error, res: Response) {
+  return `${err.message}\n\nResponse Headers: ${JSON.stringify(
+    res.headers,
+    null,
+    2
+  )}\n\nResponse Body: ${JSON.stringify(res.body, null, 2)}`;
+}
 
 describe("CollectionsController", () => {
   let app: INestApplication;
 
+  let response: Response;
+
+  // The provider in the STIX module (usually WorkbenchRepository) will be overridden with a mock provider that serves
+  // fake STIX data
   const stixRepo = new MockStixRepository();
 
+  // All mock HTTP requests must have a TAXII-compliant Accept header included
+  const commonHeaders = {
+    Accept: "application/taxii+json;version=2.1",
+  };
+
+  // Initialize the application
   beforeAll(async () => {
     const tempConfigService = await Test.createTestingModule({
       imports: [TaxiiConfigModule],
@@ -51,95 +50,274 @@ describe("CollectionsController", () => {
             .createCacheConnectOptions(),
         }),
       ],
-      // imports: [
-      //   RequestContextModule.forRoot({
-      //     contextClass: RequestContext,
-      //     isGlobal: true,
-      //   }),
-      //   TaxiiConfigModule,
-      //   TaxiiLoggerModule,
-      //   HttpModule.register({
-      //     headers: { Authorization: `Basic ${DEFAULT_WORKBENCH_AUTH_HEADER}` },
-      //   }),
-      // ],
-      // providers: [
-      //   CollectionService,
-      //   CollectionRepository,
-      //   ObjectService,
-      //   ObjectRepository,
-      //   FilterService,
-      //   PaginationService,
-      //   EnvelopeService,
-      //   ManifestService,
-      //   ManifestRecordService,
-      //   VersionService,
-      //   DiscoveryService,
-      //   {
-      //     provide: STIX_REPO_TOKEN,
-      //     useValue: WorkbenchRepository,
-      //   },
-      // ],
-      // controllers: [CollectionsController, RootController],
     })
       .overrideProvider(STIX_REPO_TOKEN)
       .useValue(stixRepo)
-      .overrideFilter(TaxiiExceptionFilter)
-      .useValue(new TaxiiExceptionFilter())
       .compile();
 
+    await tempConfigService.close();
     app = moduleRef.createNestApplication();
     await app.init();
   });
 
-  it("GET /taxii2/", () => {
-    return request(app.getHttpServer()).get("/taxii2/").expect(200);
+  /**
+   * 1.6.8 Content Negotiation
+   * TAXII Clients and Servers must use correct Accept and Content-Type headers to negotiate TAXII versions
+   * Sends a request with a bad Accept header -> expect 406 response
+   */
+  it("Content Negotiation - Bad Accept Header", (done) => {
+    request(app.getHttpServer())
+      .get("/taxii2/")
+      .set({ Accept: "not-a-real-media-type" })
+      .end((err, res) => {
+        try {
+          expect(res.statusCode).toEqual(406);
+        } catch (err) {
+          err.message = formatError(err, res);
+          throw err;
+        }
+        done();
+      });
   });
 
-  it("GET /collections/", () => {
-    return request(app.getHttpServer()).get("/collections/").expect(200);
+  /**
+   * 4.1 Server Discovery
+   */
+  it("Server Discovery :: GET /taxii2/", (done) => {
+    request(app.getHttpServer())
+      .get("/taxii2/")
+      .set(commonHeaders)
+      .end((err, res) => {
+        try {
+          expect(res.statusCode).toEqual(200);
+          expect(res.body).toEqual({ ...app.get(DiscoveryService).discover() });
+        } catch (err) {
+          err.message = formatError(err, res);
+          throw err;
+        }
+        done();
+      });
   });
 
-  it("GET /collections/{collection-id}/", () => {
-    return request(app.getHttpServer())
+  /**
+   * 4.2 Get API Root Information
+   */
+  it("Get API Root Information :: GET {api-root}", (done) => {
+    request(app.getHttpServer())
+      .get("/")
+      .set(commonHeaders)
+      .end((err, res) => {
+        try {
+          expect(res.statusCode).toEqual(200);
+          expect(res.body).toEqual(
+            app.get(DiscoveryService).findApiRootInformation()
+          );
+        } catch (err) {
+          err.message = formatError(err, res);
+          throw err;
+        }
+        done();
+      });
+  });
+
+  /**
+   * 5.1 Get Collections
+   */
+  it("Get Collections :: GET {api-root}/collections/", (done) => {
+    request(app.getHttpServer())
+      .get("/collections/")
+      .set(commonHeaders)
+      .end((err, res) => {
+        try {
+          expect(res.statusCode).toEqual(200);
+          expect(res.headers["content-type"]).toBeDefined();
+          expect(res.headers["content-type"]).toEqual(
+            "application/taxii+json; charset=utf-8; version=2.1"
+          );
+        } catch (err) {
+          err.message = formatError(err, res);
+          throw err;
+        }
+        done();
+      });
+  });
+
+  /**
+   * 5.2 Get a Collection
+   */
+  it("Get a Collection :: GET {api-root}/collections/{collection-id}/", (done) => {
+    request(app.getHttpServer())
       .get("/collections/mock-collection-id/")
-      .expect(200);
+      .set(commonHeaders)
+      .end((err, res) => {
+        try {
+          expect(res.statusCode).toEqual(200);
+          expect(res.headers["content-type"]).toBeDefined();
+          expect(res.headers["content-type"]).toEqual(
+            "application/taxii+json; charset=utf-8; version=2.1"
+          );
+        } catch (err) {
+          err.message = formatError(err, res);
+          throw err;
+        }
+        done();
+      });
   });
 
-  it("GET /collections/{collection-id}/manifest/", () => {
-    return request(app.getHttpServer())
+  /**
+   * 5.3 Get Object Manifests
+   */
+  it("Get Object Manifests :: GET {api-root}/collections/{collection-id}/manifest/", (done) => {
+    request(app.getHttpServer())
       .get("/collections/mock-collection-id/manifest/")
-      .expect(200);
+      .set(commonHeaders)
+      .end((err, res) => {
+        try {
+          expect(res.statusCode).toEqual(200);
+          expect(res.headers["content-type"]).toBeDefined();
+          expect(res.headers["content-type"]).toEqual(
+            "application/taxii+json; charset=utf-8; version=2.1"
+          );
+          expect(res.headers["x-taxii-date-added-first"]).toBeDefined();
+          expect(res.headers["x-taxii-date-added-last"]).toBeDefined();
+        } catch (err) {
+          err.message = formatError(err, res);
+          throw err;
+        }
+        done();
+      });
   });
 
-  it("GET /collections/{collection-id}/providers/", () => {
-    return request(app.getHttpServer())
-      .get("/collections/mock-collection-id/providers/")
-      .expect(200);
+  /**
+   * 5.4 Get Objects
+   */
+  it("Get Objects :: GET {api-root}/collections/{collection-id}/objects/", (done) => {
+    request(app.getHttpServer())
+      .get("/collections/mock-collection-id/objects/")
+      .set(commonHeaders)
+      .end((err, res) => {
+        try {
+          expect(res.statusCode).toEqual(200);
+          expect(res.headers["content-type"]).toBeDefined();
+          expect(res.headers["content-type"]).toEqual(
+            "application/taxii+json; charset=utf-8; version=2.1"
+          );
+          expect(res.headers["x-taxii-date-added-first"]).toBeDefined();
+          expect(res.headers["x-taxii-date-added-last"]).toBeDefined();
+        } catch (err) {
+          err.message = formatError(err, res);
+          throw err;
+        }
+        done();
+      });
   });
 
-  it("GET /collections/{collection-id}/providers/{object-id}/", () => {
-    return request(app.getHttpServer())
-      .get("/collections/mock-collection-id/providers/mock-object-id/")
-      .expect(200);
+  /**
+   * 5.6 Get an Object
+   */
+  it("Get an Object :: GET {api-root}/collections/{collection-id}/objects/{object-id}/", (done) => {
+    request(app.getHttpServer())
+      .get("/collections/mock-collection-id/objects/mock-object-id/")
+      .set(commonHeaders)
+      .end((err, res) => {
+        try {
+          expect(res.statusCode).toEqual(200);
+          expect(res.headers["content-type"]).toBeDefined();
+          expect(res.headers["content-type"]).toEqual(
+            "application/taxii+json; charset=utf-8; version=2.1"
+          );
+          expect(res.headers["x-taxii-date-added-first"]).toBeDefined();
+          expect(res.headers["x-taxii-date-added-last"]).toBeDefined();
+        } catch (err) {
+          err.message = formatError(err, res);
+          throw err;
+        }
+        done();
+      });
   });
 
-  // it("POST /collections/{collection-id}/providers/", () => {
-  //   return request(app.getHttpServer())
-  //     .post("/collections/mock-collection-id/providers/")
-  //     .expect(503);
-  // });
+  /**
+   * 5.5 Add Objects
+   */
+  it("Add Objects :: POST {api-root}/collections/{collection-id}/objects/", (done) => {
+    request(app.getHttpServer())
+      .post("/collections/mock-collection-id/objects/")
+      .set(commonHeaders)
+      .end((err, res) => {
+        try {
+          expect(res.statusCode).toEqual(503);
+        } catch (err) {
+          err.message = formatError(err, res);
+          throw err;
+        }
+        done();
+      });
+  });
 
-  // it("DELETE /collections/{collection-id}/providers/{object-id}/", () => {
-  //   return request(app.getHttpServer())
-  //     .delete("/collections/mock-collection-id/providers/mock-object-id/")
-  //     .expect(503);
-  // });
-  //
-  // it("GET /collections/{collection-id}/providers/{object-id}/versions/", () => {
-  //   return request(app.getHttpServer())
-  //     .get("/collections/mock-collection-id/providers/mock-object-id/versions/")
-  //     .expect(200);
-  // });
+  /**
+   * 5.7 Delete an Object
+   */
+  it("Delete an Object :: DELETE {api-root}/collections/{collection-id}/objects/{object-id}/", (done) => {
+    request(app.getHttpServer())
+      .delete("/collections/mock-collection-id/objects/mock-object-id/")
+      .set(commonHeaders)
+      .end((err, res) => {
+        try {
+          expect(res.statusCode).toEqual(503);
+        } catch (err) {
+          err.message = formatError(err, res);
+          throw err;
+        }
+        done();
+      });
+  });
+
+  /**
+   * 5.8 Get Object Versions
+   */
+  it("Get Object Versions :: GET {api-root}/collections/{collection-id}/objects/{object-id}/versions/", (done) => {
+    request(app.getHttpServer())
+      .get("/collections/mock-collection-id/objects/mock-object-id/versions/")
+      .set(commonHeaders)
+      .end((err, res) => {
+        try {
+          expect(res.statusCode).toEqual(200);
+          expect(res.headers["content-type"]).toBeDefined();
+          expect(res.headers["content-type"]).toEqual(
+            "application/taxii+json; charset=utf-8; version=2.1"
+          );
+          expect(res.headers["x-taxii-date-added-first"]).toBeDefined();
+          expect(res.headers["x-taxii-date-added-last"]).toBeDefined();
+        } catch (err) {
+          err.message = formatError(err, res);
+          throw err;
+        }
+        done();
+      });
+  });
+
+  it("Pagination is working", (done) => {
+    request(app.getHttpServer())
+      .get("/collections/mock-collection-id/objects/?limit=5")
+      .set(commonHeaders)
+      .end((err, res) => {
+        try {
+          expect(res.statusCode).toEqual(200);
+          expect(res.headers["content-type"]).toBeDefined();
+          expect(res.headers["content-type"]).toEqual(
+            "application/taxii+json; charset=utf-8; version=2.1"
+          );
+          expect(res.headers["x-taxii-date-added-first"]).toBeDefined();
+          expect(res.headers["x-taxii-date-added-last"]).toBeDefined();
+          expect(res.body.objects).toBeDefined(); // <-- res.body is an instance of EnvelopeDto
+          expect(res.body.objects).toHaveLength(5);
+        } catch (err) {
+          err.message = formatError(err, res);
+          throw err;
+        }
+        done();
+      });
+  });
 
   afterAll(async () => {
     await app.close();

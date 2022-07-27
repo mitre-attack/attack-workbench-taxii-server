431ce1357b2809fbcd11b8558ad8b2ceb73ce885
Sean Sica <23294618+seansica@users.noreply.github.com>
2022-07-27 11:49:09 -0400

    resolved nettimeout propagation issue in memcache-plus source repository

--- src/cache/taxii-cache.service.ts
+++ src/cache/taxii-cache.service.ts
@@ -1,10 +1,9 @@
 import { CACHE_OPTIONS } from "./constants";
-import { CacheModule, DynamicModule, Inject, Injectable } from "@nestjs/common";
-// import { CACHE_CONNECT_OPTIONS } from "./taxii-cache.module";
+import { CacheModule, DynamicModule, Injectable } from "@nestjs/common";
 import { Cache } from "cache-manager";
 
 // ** memcached dependencies ** //
-import * as Memcache from "memcache-pp";
+import * as Memcache from "memcache-plus";
 import * as memcachedStore from "cache-manager-memcached-store";
 
 @Injectable()

323ea907ab297a811df362d5488508e5d10c823d
Sean Sica <23294618+seansica@users.noreply.github.com>
2022-08-10 07:34:53 -0400

    decorated DTO with Swagger annotations

--- src/taxii/providers/envelope/dto/envelope.dto.ts
+++ src/taxii/providers/envelope/dto/envelope.dto.ts
@@ -7,6 +7,8 @@ import {
   GenericPageDto,
   GenericPageOptions,
 } from "../../pagination/dto/generic-page.dto";
+import { ApiProperty } from "@nestjs/swagger";
+import { SwaggerDocumentation as SWAGGER } from "./envelope.dto.swagger.json";
 
 export interface EnvelopeConstructorOptions
   extends GenericPageOptions<StixObjectPropertiesInterface> {
@@ -24,16 +26,32 @@ export class EnvelopeDto
   @Exclude()
   id: string;
 
+  @ApiProperty({
+    description: SWAGGER.More.description,
+    type: SWAGGER.More.type,
+    required: SWAGGER.More.required,
+  })
   @Expose()
   @IsOptional()
   @IsBoolean()
   more: boolean;
 
+  @ApiProperty({
+    description: SWAGGER.Next.description,
+    type: SWAGGER.Next.type,
+    required: SWAGGER.Next.required,
+  })
   @Expose()
   @IsString()
   @IsOptional()
   next: string;
 
+  @ApiProperty({
+    description: SWAGGER.Objects.description,
+    type: SWAGGER.Objects.type,
+    required: SWAGGER.Objects.required,
+    name: "objects",
+  })
   @IsOptional()
   @Type(() => WorkbenchStixObjectPropertiesDto)
   @Expose({ name: "objects" })

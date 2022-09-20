41cb7913a7505eaa07b38471bc4376f0b1946833
Sean Sica <23294618+seansica@users.noreply.github.com>
2022-09-20 15:02:14 -0400

    removed un-used imports and providers from module dependencies

--- src/taxii/providers/object/object.module.ts
+++ src/taxii/providers/object/object.module.ts
@@ -3,24 +3,19 @@ import { ObjectService } from "./object.service";
 import { FilterModule } from "../filter/filter.module";
 import { ObjectRepository } from "./object.repository";
 import { MongooseModule } from "@nestjs/mongoose";
-import { CollectionModule } from "../collection";
 import {
   AttackObjectSchema,
   AttackObject,
 } from "src/hydrate/collector/schema/attack-object.schema";
-import { StixModule } from "src/stix/stix.module";
-import { ObjectCollectorService } from "src/hydrate/collector/providers/object-collector.service";
 
 @Module({
   imports: [
-    StixModule,
     FilterModule,
     MongooseModule.forFeature([
       { name: AttackObject.name, schema: AttackObjectSchema },
     ]),
-    CollectionModule,
   ],
-  providers: [ObjectService, ObjectRepository, ObjectCollectorService],
+  providers: [ObjectService, ObjectRepository],
   exports: [ObjectService],
 })
 export class ObjectModule {}

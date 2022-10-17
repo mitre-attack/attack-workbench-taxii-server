0ec0c93e25ce5c9d0369d95b4df201b70e09989a
Sean Sica <23294618+seansica@users.noreply.github.com>
2022-10-17 10:45:31 -0400

    add backwards compatibility for STIX 2.0

--- src/hydrate/collector/providers/object-collector.service.ts
+++ src/hydrate/collector/providers/object-collector.service.ts
@@ -8,6 +8,28 @@ import { AttackObject } from "../schema";
 import { STIX_REPO_TOKEN } from "../../../stix/constants";
 import { StixRepositoryInterface } from "../../../stix/providers/stix.repository.interface";
 import { GET_ATTACK_OBJECTS_JOB_TOKEN } from "../constants";
+import { StixObjectDto } from "../../../stix/dto/stix-object.dto";
+
+const stixOptionalArrayProperties = [
+  "x_mitre_aliases",
+  "x_mitre_contributors",
+  "x_mitre_data_sources",
+  "x_mitre_defense_bypassed",
+  "x_mitre_domains",
+  "x_mitre_effective_permissions",
+  "x_mitre_impact_type",
+  "x_mitre_system_requirements",
+  "x_mitre_permissions_required",
+  "x_mitre_platforms",
+  "x_mitre_remote_support",
+  "x_mitre_tactic_type",
+  "external_references",
+  "kill_chain_phases",
+  "aliases",
+  "object_marking_refs",
+  "roles",
+  "sectors",
+];
 
 @Injectable()
 export class ObjectCollectorService {
@@ -38,9 +60,39 @@ export class ObjectCollectorService {
 
     this.logger.debug(`Retrieved ${allAttackObjects.length} ATT&CK objects`);
 
+    /**
+     * Pass the STIX-2.1 objects to the transform function.
+     * The transform function should return a new array of size 2(n).
+     * Half of the elements will be STIX-2.0 objects. The other half should be the original/unchanged set of
+     * STIX-2.1 objects.
+     */
+    const objectsToWriteToDatabase = [];
+
+    allAttackObjects.map((attackObject) => {
+      /**
+       * First, generate an equivalent STIX-2.0 object for every STIX-2.1 object in the dataset. Store the generated
+       * object in a temporary array. We'll merge it back in later.
+       */
+      const stix_20_Object = this.convertStixObjectToVersion(
+        <AttackObjectDto>attackObject,
+        "2.0"
+      );
+      objectsToWriteToDatabase.push(stix_20_Object);
+
+      const stix_21_Object = this.convertStixObjectToVersion(
+        <AttackObjectDto>attackObject,
+        "2.1"
+      );
+      objectsToWriteToDatabase.push(stix_21_Object);
+    });
+    /**
+     * Now merge the STIX-2.1 objects with the STIX-2.0 objects. All of them will be stored in the database.
+     */
+    // allAttackObjects = [...objectsToWriteToDatabase, ...allAttackObjects];
+
     // Sort the list of objects returned from Workbench now, before we write them to the database; pre-sorting the mongo
     // collection now means we don't need to sort TAXII responses later
-    allAttackObjects.sort((a, b) => {
+    objectsToWriteToDatabase.sort((a, b) => {
       const createdA = new Date(a.stix.created).valueOf();
       const createdB = new Date(b.stix.created).valueOf();
       if (createdA < createdB) {
@@ -52,9 +104,13 @@ export class ObjectCollectorService {
       return 0;
     });
 
+    this.logger.debug(
+      `Processed ${objectsToWriteToDatabase.length} ATT&CK objects`
+    );
+
     // Write each object to the database. Creates a new document if the object does not already exist. Otherwise updates
     // the existing document.
-    for (const object of allAttackObjects) {
+    for (const object of objectsToWriteToDatabase) {
       if ((<AttackObjectDto>object).workspace.collections.length >= 1) {
         // only operate on the object if the object has an ATT&CK ID
 
@@ -67,6 +123,7 @@ export class ObjectCollectorService {
           "stix.id": { $eq: object.stix.id },
           "stix.modified": { $eq: object.stix.modified },
           "stix.created": { $eq: object.stix.created },
+          "stix.spec_version": { $eq: object.stix.spec_version },
         };
         await this.stixObjectModel
           .updateOne(
@@ -77,7 +134,8 @@ export class ObjectCollectorService {
             },
             {
               upsert: true,
-              strict: false, // strict mode allows us to capture properties not explicitly declared in the schema
+              strict: false, // disabling strict mode allows us to capture properties not explicitly declared in the
+              // schema
             }
           )
           .exec();
@@ -90,5 +148,63 @@ export class ObjectCollectorService {
 
     // end for
     this.logger.debug("Completed database object hydration");
+  }
+
+  convertStixObjectToVersion(
+    attackObject: AttackObjectDto,
+    version: string
+  ): AttackObjectDto {
+    const returnObject = JSON.parse(JSON.stringify(attackObject));
+    if (version === "2.0") {
+      // eslint-disable-next-line no-prototype-builtins
+      if (returnObject.stix.hasOwnProperty("spec_version")) {
+        returnObject.stix.spec_version = undefined;
+      }
+      // returnObject.stix.spec_version = "2.0"; // TODO review whether this is acceptable
+
+      if (returnObject.stix.type === "malware") {
+        // STIX 2.0 malware may not have the property is_family
+        // eslint-disable-next-line no-prototype-builtins
+        if (returnObject.stix.hasOwnProperty("is_family")) {
+          returnObject.stix["is_family"] = undefined;
+        }
+
+        // STIX 2.0 malware must have the property labels
+        returnObject.stix["labels"] = ["malware"];
+      }
+
+      if (returnObject.stix.type === "tool") {
+        // STIX 2.0 tools may not have the property is_family
+        // eslint-disable-next-line no-prototype-builtins
+        if (returnObject.stix.hasOwnProperty("is_family")) {
+          returnObject.stix["is_family"] = undefined;
+        }
+
+        // STIX 2.0 tools must have the property labels
+        returnObject.stix.labels = ["tool"];
+      }
+    } else if (version == "2.1") {
+      returnObject.stix.spec_version = "2.1";
+
+      // STIX 2.1 malware must have the property is_family
+      if (returnObject.stix.type === "malware") {
+        returnObject.stix["is_family"] = attackObject.stix["is_family"] ?? true;
+      }
+    }
+
+    this.removeEmptyArrays(returnObject.stix, stixOptionalArrayProperties);
+
+    return returnObject;
+  }
+
+  removeEmptyArrays(attackObject: StixObjectDto, propertyNames) {
+    for (const propertyName of propertyNames) {
+      if (
+        Array.isArray(attackObject[propertyName]) &&
+        attackObject[propertyName].length === 0
+      ) {
+        delete attackObject[propertyName];
+      }
+    }
   }
 }

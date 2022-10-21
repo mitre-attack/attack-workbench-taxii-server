45a53ed2c4a8c248216471926fd99aeb949f03ed
Sean Sica <23294618+seansica@users.noreply.github.com>
2022-10-21 14:15:52 -0400

    rename and refactor content negotiation middleware

--- src/taxii/taxii.module.ts
+++ src/taxii/taxii.module.ts
@@ -8,9 +8,9 @@ import { APP_INTERCEPTOR } from "@nestjs/core";
 
 // ** middleware ** //
 import { SetRequestIdMiddleware } from "src/common/middleware/set-request-id.middleware";
-import { ValidateRequestHeadersMiddleware } from "src/common/middleware/validate-request-headers.middleware";
+import { ContentNegotiationMiddleware } from "src/common/middleware/content-negotiation";
 import { ResLoggerMiddleware } from "src/common/middleware/res-logger.middleware";
-import { SetMediaType } from "src/common/interceptors/set-media-type.interceptor";
+import { SetResponseMediaType } from "src/common/interceptors/set-response-media-type.interceptor";
 
 //** controllers **//
 import { CollectionsController } from "src/taxii/controllers/collections/collections.controller";
@@ -39,14 +39,14 @@ import {
   ],
   controllers: [CollectionsController, RootController],
   providers: [
-    { provide: APP_INTERCEPTOR, useClass: SetMediaType },
+    { provide: APP_INTERCEPTOR, useClass: SetResponseMediaType },
     { provide: APP_INTERCEPTOR, useClass: ClassSerializerInterceptor },
   ],
 })
 export class TaxiiModule implements NestModule {
   configure(consumer: MiddlewareConsumer): any {
     consumer.apply(SetRequestIdMiddleware).forRoutes("*"); // Generate a unique ID for each request
-    consumer.apply(ValidateRequestHeadersMiddleware).forRoutes("*"); // Inspect Accept header on all requests
+    consumer.apply(ContentNegotiationMiddleware).forRoutes("*"); // Inspect Accept header on all requests
     consumer.apply(ResLoggerMiddleware).forRoutes("*"); // Log each request
   }
 }

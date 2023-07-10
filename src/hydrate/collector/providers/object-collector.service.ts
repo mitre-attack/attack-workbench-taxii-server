a7146ad81120673429cf6ca848934333193f4763
Sean Sica <23294618+seansica@users.noreply.github.com>
2023-07-10 16:57:22 -0400

    Refactor object-collector.service to bulkUpdate the database (instead of using updateOne per doc)

--- src/hydrate/collector/providers/object-collector.service.ts
+++ src/hydrate/collector/providers/object-collector.service.ts
@@ -38,8 +38,7 @@ export class ObjectCollectorService {
   );
 
   constructor(
-    @Inject(STIX_REPO_TOKEN)
-    private readonly workbench: StixRepositoryInterface,
+    @Inject(STIX_REPO_TOKEN) private readonly workbench: StixRepositoryInterface,
     @InjectModel(AttackObject.name) private stixObjectModel: Model<AttackObject>
   ) {}
 
@@ -55,8 +54,7 @@ export class ObjectCollectorService {
      * 2. For each collection ID, request the respective collection bundle
      * 3. Push { collection_id, stix } for each object retrieved
      */
-    const allAttackObjects: StixObjectInterface[] =
-      await this.workbench.getAllStixObjects(false);
+    const allAttackObjects: StixObjectInterface[] = await this.workbench.getAllStixObjects(false);
 
     this.logger.debug(`Retrieved ${allAttackObjects.length} ATT&CK objects`);
 
@@ -108,8 +106,10 @@ export class ObjectCollectorService {
       `Processed ${objectsToWriteToDatabase.length} ATT&CK objects`
     );
 
-    // Write each object to the database. Creates a new document if the object does not already exist. Otherwise updates
+    // Write object to the database using bulk operation. Creates a new document if the object does not already exist. Otherwise updates
     // the existing document.
+    let bulkOps = [];
+
     for (const object of objectsToWriteToDatabase) {
       if ((<AttackObjectDto>object).workspace.collections.length >= 1) {
         // only operate on the object if the object has an ATT&CK ID
@@ -125,25 +125,36 @@ export class ObjectCollectorService {
           "stix.created": { $eq: object.stix.created },
           "stix.spec_version": { $eq: object.stix.spec_version },
         };
-        await this.stixObjectModel
-          .updateOne(
-            filter,
-            {
-              collection_id: collectionId,
-              stix: object.stix,
+        // push the updateOne operation into bulkOps array
+        bulkOps.push({
+          updateOne: {
+            filter: filter,
+            update: {
+              $set: {
+                collection_id: collectionId,
+                stix: object.stix,
+              },
             },
-            {
-              upsert: true,
-              strict: false, // disabling strict mode allows us to capture properties not explicitly declared in the
-              // schema
-            }
-          )
-          .exec();
+            upsert: true,
+            strict: false, // disabling strict mode allows us to capture properties not explicitly declared in the schema
+          }
+        });
+        // execute bulkWrite each 1000 operations (or at the end of the loop)
+        if (bulkOps.length === 1000) {
+          await this.stixObjectModel.bulkWrite(bulkOps);
+          bulkOps = []; // clear the array
+        }
+
         this.logger.debug(
           `Synchronized STIX object '${object.stix.id}' to TAXII database`
         );
       }
       // end if
+    }
+
+    // execute the remaining operations, if any
+    if (bulkOps.length > 0) {
+      await this.stixObjectModel.bulkWrite(bulkOps);
     }
 
     // end for

53aa4574988739d0b69b86bdf1973a228c383f2d
Sean Sica <23294618+seansica@users.noreply.github.com>
2023-07-10 16:47:38 -0400

    update docker-build.sh to load dotenv file before execution

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,6 +2,14 @@
 
 cd ..
 
+TAXII_ENV=dev
+
+# Load environment variables from dotenv file
+# The set -a command means that any variable or function that is defined or changed in the .env file will be exported. This means these variables will be available to any child processes spawned from the script. The set +a command then turns off the automatic export of variables, preventing variables defined later in the script from being automatically exported.
+set -a
+source config/${TAXII_ENV}.env
+set +a
+
 docker build \
   --no-cache \
   --build-arg TAXII_ENV="${TAXII_ENV}" \

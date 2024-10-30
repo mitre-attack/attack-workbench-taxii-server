6e4eef8530c310d705800d4264a2d34356ceb770
Sean Sica <23294618+seansica@users.noreply.github.com>
2024-10-30 18:58:41 -0400

    fix: resolve bug causing class-transformer to serialize dates to empty objects
    - Any response objects of type Date were serializing to empty objects/dicts (e.g., {})
    - The bug was manifested in the snake-case interceptor
    - Added handling for dates to the snake-case interceptor
    - Verified all endpoints behaving correctly now

--- src/common/interceptors/snake-case.interceptor.ts
+++ src/common/interceptors/snake-case.interceptor.ts
@@ -10,23 +10,33 @@ export class SnakeCaseInterceptor implements NestInterceptor {
         );
     }
 
-    private transform(data: any) {
+    private transform(data: any): any {
         if (Array.isArray(data)) {
             return data.map(item => this.transformToSnakeCase(item));
         }
         return this.transformToSnakeCase(data);
     }
 
-    private transformToSnakeCase(data: any) {
+    private transformToSnakeCase(data: any): any {
         if (typeof data !== 'object' || data === null) {
             return data;
+        }
+
+        if (data instanceof Date) {
+            // Return the date as an ISO string without further transformation
+            return data.toISOString();
+        }
+
+        if (Array.isArray(data)) {
+            return data.map(item => this.transformToSnakeCase(item));
         }
 
         const snakeCaseData = {};
         for (const key in data) {
             if (Object.prototype.hasOwnProperty.call(data, key)) {
+                const value = data[key];
                 const snakeKey = key.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
-                snakeCaseData[snakeKey] = this.transform(data[key]);
+                snakeCaseData[snakeKey] = this.transformToSnakeCase(value);
             }
         }
         return snakeCaseData;

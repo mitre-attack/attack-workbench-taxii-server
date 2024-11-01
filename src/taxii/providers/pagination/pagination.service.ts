cd739f6da2378ccd0a1a1eb01eade51a89b04398
Sean Sica <23294618+seansica@users.noreply.github.com>
2024-11-01 12:58:36 -0400

    clean: format pagination.service

--- src/taxii/providers/pagination/pagination.service.ts
+++ src/taxii/providers/pagination/pagination.service.ts
@@ -1,13 +1,10 @@
 import { Injectable } from "@nestjs/common";
 import { TaxiiLoggerService as Logger } from "src/common/logger";
-import "object-hash";
 import { TaxiiNotFoundException } from "src/common/exceptions";
-import { StixObjectPropertiesInterface } from "src/stix/interfaces/stix-object-properties.interface";
 import { EnvelopeDto } from "../envelope";
 import { isNumber } from "@nestjs/common/utils/shared.utils";
 import { ManifestDto, ManifestRecordDto } from "../manifest/dto";
 import { VersionDto } from "../version/dto/version.dto";
-import { GenericPageDto } from "./dto/generic-page.dto";
 import { StixObjectDto } from "src/stix/dto/stix-object.dto";
 
 @Injectable()
@@ -19,10 +16,25 @@ export class PaginationService {
   /**
    * Handles pagination of STIX object, manifest-record, and version string arrays. This is where all pagination logic
    * lives. The other class methods (which are public) call this method to process pages. They themselves just
-   * transform the generic response object (an instance of GenericPageDto) to their respective resource types.
-   * @param objects A list of either: ManifestRecordDto (if being called by the ManifestService), string (if being
-   *              called by the VersionService), StixObjectPropertiesInterface (if being called by the EnvelopeService)
-   * @param limit The number of objects that should be included on the page
+   * transform the generic response object to their respective resource types.
+   * 
+   * Example pagination:
+   * all_objects = [a,b,c,d,e,f,g], length=7
+   * limit=2, next=2
+   * resulting pages = {
+   *  page-0: [a,b],
+   *  page-1: [c,d],
+   *  page-2: [e,f], <-- this is the requested page
+   *  page-3: [g] <-- `isMore` determines if this page exists
+   * }
+   * We know there are additional pages after page-2 because:
+   * - nextPage + currentPage < totalobjects
+   * - (limit) + (limit*next) < objects.length
+   * - (2) + (2*2) < 7
+   * - 6 < 7
+   * 
+   * @param items Array of items to paginate
+   * @param limit The number of items that should be included on the page
    * @param next Specifies which page is being requested
    * @private
    */
@@ -31,7 +43,9 @@ export class PaginationService {
     limit?: number,
     next?: number
   ): Promise<{ more?: boolean; next?: string; items: T[] }> {
+    // Pagination can only occur if `limit` is defined and valid
     if (isNumber(limit)) {
+      // Handle case where next page is specified
       if (isNumber(next)) {
         if (limit * next <= items.length) {
           const isMore: boolean = limit + limit * next < items.length;
@@ -41,11 +55,14 @@ export class PaginationService {
             items: items.slice(limit * next, limit + limit * next)
           };
         }
+        // The values for `next` and `limit` are invalid for the selected items array.
+        // This happens when requesting a page that doesn't exist.
         throw new TaxiiNotFoundException({
           title: "Invalid URL Query Parameters",
           description: `The server was unable to process a page where 'next' equals ${next} and 'limit' equals ${limit}`,
         });
       }
+      // Handle first page when only limit is specified
       const isMore: boolean = limit < items.length;
       return {
         more: isMore,
@@ -53,101 +70,11 @@ export class PaginationService {
         items: items.slice(0, limit)
       };
     }
+    // Return all items if no pagination parameters specified
     return {
       items
     };
   }
-  // private async getPage(
-  //   objects: any[],
-  //   limit?: number,
-  //   next?: number
-  // ): Promise<GenericPageDto> {
-  //   // Pagination can only occur if `limit` is defined and valid.
-  //   if (isNumber(limit)) {
-  //     // A valid `limit` parameter was passed!
-  //     this.logger.debug(
-  //       `Limit is defined. Attempting to paginate via limit (${limit})`,
-  //       this.constructor.name
-  //     );
-  //     /**
-  //      * `next` is the mechanism by which we walk through pages.
-  //      * If `next` is not defined or valid, then we will just return page 1.
-  //      */
-  //     if (isNumber(next)) {
-  //       // A valid `next` parameter was passed!
-  //       this.logger.debug(
-  //         `Next is defined. Attempting to paginate via limit (${limit}) and next (${next})`,
-  //         this.constructor.name
-  //       );
-  //       /**
-  //        * The following `if` condition determines whether pagination is even possible for the supplied `objects`,
-  //        * `limit`, and `next` combination. For example, it is possible for the user to request a page that does not
-  //        * exist.
-  //        */
-  //       if (limit * next <= objects.length) {
-  //         /**
-  //          * Example:
-  //          * all_objects = [a,b,c,d,e,f,g], length=7
-  //          * limit=2, next=2
-  //          * resulting pages = {
-  //          *  page-0: [a,b],
-  //          *  page-1: [c,d],
-  //          *  page-2: [e,f], <-- this is the requested page
-  //          *  page-3: [g] <-- `isMore` determines if this page exists
-  //          *  }
-  //          *  We know there are additional pages after page-2 because:
-  //          *  - nextPage + currentPage < totalobjects
-  //          *  - (limit) + (limit*next) < objects.length
-  //          *  - (2) + (2*2) < 7
-  //          *  - 6 < 7
-  //          */
-  //         const isMore: boolean = limit + limit * next < objects.length;
-
-  //         return new GenericPageDto({
-  //           more: isMore,
-  //           next: isMore ? String(next + 1) : undefined,
-  //           objects: objects.slice(limit * next, limit + limit * next),
-  //         });
-  //       }
-
-  //       /**
-  //        * The values for `next` and `limit` are invalid for the selected objects array. Usually this happens as a result
-  //        * of the user requesting a page that does not exist.
-  //        *
-  //        * e.g., We have 10 objects and want to paginate by 5 (limit=5). Therefore, there should be two pages. However,
-  //        * the user requested page 3 (next=3), resulting in an out of bounds exception. In this case, we return a 400
-  //        * response.
-  //        */
-  //       throw new TaxiiNotFoundException({
-  //         title: "Invalid URL Query Parameters",
-  //         description: `The server was unable to process a page where 'next' equals ${next} and 'limit' equals ${limit}`,
-  //       });
-  //     }
-  //     // The else block will execute if next was undefined or invalid
-  //     else {
-  //       // Paginating by `limit`. This will only ever resolve to the first page, since `next` is the mechanism by which
-  //       // we step through pages.
-
-  //       const isMore: boolean = limit < objects.length;
-
-  //       // Return page 1
-  //       return new GenericPageDto({
-  //         more: isMore,
-  //         next: isMore ? "1" : undefined,
-  //         objects: objects.slice(0, limit),
-  //       });
-  //     }
-  //   }
-
-  //   this.logger.debug(
-  //     "Pagination bypassed. Returning page containing all available objects",
-  //     this.constructor.name
-  //   );
-  //   return new GenericPageDto({
-  //     more: false,
-  //     objects: objects,
-  //   });
-  // }
 
   /**
    * Paginates an array of STIX objects
@@ -155,13 +82,6 @@ export class PaginationService {
    * @param limit The number of objects that should be included on the page
    * @param next Specifies which page is being requested
    */
-  // async getEnvelope(
-  //   objects: StixObjectPropertiesInterface[],
-  //   limit?: number,
-  //   next?: number
-  // ): Promise<EnvelopeDto> {
-  //   return new EnvelopeDto(await this.getPage(objects, limit, next));
-  // }
   async getEnvelope(
     objects: StixObjectDto[],
     limit?: number,
@@ -181,13 +101,6 @@ export class PaginationService {
    * @param limit The number of manifest records that should be included on the page
    * @param next Specifies which page is being requested
    */
-  // async getManifest(
-  //   objects: ManifestRecordDto[],
-  //   limit?: number,
-  //   next?: number
-  // ): Promise<ManifestDto> {
-  //   return new ManifestDto(await this.getPage(objects, limit, next));
-  // }
   async getManifest(
     objects: ManifestRecordDto[],
     limit?: number,
@@ -203,34 +116,10 @@ export class PaginationService {
 
   /**
    * Paginates an array of object versions
-   * @param objects The array of object versions (which are just strings) that should be paginated
+   * @param versions The array of object versions (which are just strings) that should be paginated
    * @param limit The number of versions that should be included on the page
    * @param next Specifies which page is being requested
    */
-  // async getVersion(
-  //   objects: string[],
-  //   limit?: number,
-  //   next?: number
-  // ): Promise<VersionDto> {
-  //   const genericPageDto = await this.getPage(objects, limit, next)
-  //   console.log(genericPageDto);
-  //   // GenericPageDto {
-  //   //   id: 'da8b5b9a-0ee3-441c-ad53-9b34aaac3539',
-  //   //   more: false,
-  //   //   next: undefined,
-  //   //   _objects: [ '2023-08-17T19:51:14.195Z', '2023-08-17T19:51:14.195Z' ]
-  //   // }
-  //   const versionDto = new VersionDto(genericPageDto);
-  //   // VersionDto {
-  //   //   id: '4f25aa44-70ed-4b5d-becb-b1c16182a9e4',
-  //   //   more: false,
-  //   //   next: undefined,
-  //   //   _objects: []
-  //   // }
-  //   console.log(versionDto);
-
-  //   return versionDto;
-  // }
   async getVersion(
     versions: string[],
     limit?: number,

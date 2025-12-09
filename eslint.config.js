06cc38f8598dc282f92ddae1c2afab25831a4f84
Sean Sica <23294618+seansica@users.noreply.github.com>
2025-12-09 12:37:41 -0500

    build(npm): install typescript-eslint and remove eslint mdx plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,4 @@
 import pluginJs from '@eslint/js';
-import * as mdx from 'eslint-plugin-mdx';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
@@ -13,19 +12,4 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
-
-  // MDX file support (in the documentation)
-  {
-    files: ['**/*.mdx'],
-    processor: mdx.processors.mdx,
-  },
-  {
-    files: ['**/*.{md,mdx}'],
-    plugins: {
-      mdx,
-    },
-    rules: {
-      'mdx/no-unused-expressions': 'error',
-    },
-  },
 ];

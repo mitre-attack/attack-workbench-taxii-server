5c216dd5f08e3af13e3966dfd97052d93d728154
Sean Sica <23294618+seansica@users.noreply.github.com>
2025-12-11 08:32:55 -0500

    fix: construct uuid using standard library 'crypto' instead of 'uuid'

--- src/common/models/identifier/identifier.dto.ts
+++ src/common/models/identifier/identifier.dto.ts
@@ -1,12 +1,12 @@
 import { IsString } from 'class-validator';
-import { v4 as uuid } from 'uuid';
+import { randomUUID } from 'crypto';
 
 export class IdentifierDto {
   @IsString()
   private readonly uuid: string;
 
   constructor(id?: unknown) {
-    this.uuid = id ? id.toString() : uuid(); // '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+    this.uuid = id ? id.toString() : randomUUID(); // '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
   }
 
   toString() {
